931de610fb0723a1dae3152b8cb7bc519f9ba38b
ImaginarioColectivo <marjourzua@gmail.com>
2017-12-14 11:09:12 -0300

    commit ultima version

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,42 @@
-/* query vista splash*/
-$().ready(function() {
-        $("#splash").fadeIn(500).fadeOut(2500);
-      });
+/* query vista splash que no funciona*/
+$(document).ready(function() {
+  $(".vistaSplash").fadeIn(2000).fadeOut(800,showPage);
+
+	function showPage (){
+	$('.ingreso').show();
+	}
+
+/* query para que aparezca la 2da pagina*/ 
+	$('#btnSingUpDos').click(function(){
+	  $('.ingreso').hide(); 
+	  $('.secondPage').show();
+	});
+
+/* query para dropdown 2da pagina*/ 
+	$('.search-panel .dropdown-menu').find('a').click(function(e) {
+	e.preventDefault();
+	var param = $(this).attr("href").replace("#", "");
+	var concept = $(this).text();
+	$('.search-panel span#search_concept').text(concept);
+	$('.input-group #search_param').val(param);
+
+});
+
+
+/* query deshabilitar boton next*/ 	
+
+	$('#number').keyup(function() {
+     var numeroFono = $('#number').val();
+     if (numeroFono.length === 10) {
+         $('#btnNext').removeClass("disabled");
+         $('#btnNext').addClass("waves-effect waves-light btn");
+     }
+     if (numeroFono.length > 10) {
+     $('#btnNext').addClass("disabled");
+ }
+ });
+
+/* final del jquery*/ 
+});
+
+
